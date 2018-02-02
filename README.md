a22f0ed6002e70a61b4e36f53831021efc0af8db
Sourabh Kushwaha <sourabh.kushwaha@asteria.co.in>
2018-02-02 12:42:43 +0530

    Adds code for version display using gitTag
    Signed-off-by: Sourabh Kushwaha <sourabh.kushwaha@asteria.co.in>

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DummyJenkins
+
+* Use below cmd for tagging
+git tag -a v0.X -m "my version 0.X"
+git push origin v0.X
--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
 
+#ifdef VERSION
+    QString ver = QString("%1").arg(VERSION);
+    app.setApplicationVersion(ver);
+    qDebug() <<"Version :"<< ver;
+#endif
+
     QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("qApp", qApp);
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
+
 
     return app.exec();
 }
